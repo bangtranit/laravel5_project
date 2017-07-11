f453f864c7e05b6d0c846f42b120c7c7f5b26861
TRAN THANH BANG <bangtran@neo-carreer.co.jp>
2017-07-11 22:00:57 +0900

    Commit setup project done.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Simple Blog</a>
+			</div>
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="#">New Article</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div></nav>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2>Bai viet so 1</h2>
+				<p> I have installed Apache, PHP, and MySQL on Mac OS X since Leopard. Each time doing so by hand. Each version of Mac OS X having some minor difference.</p>
+				<p><a href="#">Read More</a></p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2>Bai viet so 2</h2>
+				<p> I have installed Apache, PHP, and MySQL on Mac OS X since Leopard. Each time doing so by hand. Each version of Mac OS X having some minor difference.</p>
+				<p><a href="#">Read More</a></p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2>Bai viet so 3</h2>
+				<p> I have installed Apache, PHP, and MySQL on Mac OS X since Leopard. Each time doing so by hand. Each version of Mac OS X having some minor difference.</p>
+				<p><a href="#">Read More</a></p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2>Bai viet so 4</h2>
+				<p> I have installed Apache, PHP, and MySQL on Mac OS X since Leopard. Each time doing so by hand.</p>
+				<p><a href="#">Read More</a></p>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="container" id="footer">
+		<div class="row text-center">
+			<div class="col-sm-6 col-sm-offset-3">
+				<p>Copyright &copy;2017, by NoName</p>
+			</div>
+		</div>
+	</div>
+
+	<script src="/js/jquery.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
